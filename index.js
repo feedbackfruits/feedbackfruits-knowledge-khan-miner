620cf45522dadac570dcd2d02a02b3d6c5cc2386
Joost Verdoorn <jpverdoorn@gmail.com>
2017-03-13 02:15:41 +0100

    Push quads

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
+require('dotenv').load({ silent: true });
+
+// const KHAN_API_ENDPOINT = 'https://academic.microsoft.com/api/browse/GetEntityDetails';
+
+const {
+  NAME = 'khan-academy',
+  KAFKA_ADDRESS = 'tcp://kafka:9092',
+  OUTPUT_TOPIC = 'quad_update_requests',
+} = process.env;
+
+const memux = require('memux');
+const fetch = require('node-fetch');
+const PQueue = require('p-queue');
+const jsonld = require('jsonld').promises;
+const log = console.log.bind(console);
+
+const { send } = memux({
+  name: NAME,
+  url: KAFKA_ADDRESS,
+  output: OUTPUT_TOPIC
+});
+
+const queue = new PQueue({
+  concurrency: 16
+});
+
 const topictree = require('./topictree.json');
+
+const Context = {
+  type: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>',
+  name: '<http://schema.org/name>',
+  image: '<http://schema.org/image>',
+  description: '<http://schema.org/description>',
+  text: '<http://schema.org/text>',
+  url: '<http://schema.org/url>',
+  sameAs: '<http://schema.org/sameAs>',
+  license: '<http://schema.org/license>',
+  author: '<http://schema.org/author>',
+  about: '<http://schema.org/about>',
+  citation: '<http://schema.org/citation>',
+  CreativeWork: '<http://schema.org/CreativeWork>',
+  Movie: '<http://schema.org/Movie>',
+  Person: '<http://schema.org/Person>',
+  ReadAction: '<http://schema.org/ReadAction>',
+  WriteAction: '<http://schema.org/WriteAction>',
+
+  KhanAcademy: {
+    Topic: '<https://www.khanacademy.org/Topic>',
+    childTopic: '<https://www.khanacademy.org/childTopic>',
+    parentTopic: '<https://www.khanacademy.org/parentTopic>'
+  },
+
+  AcademicGraph: {
+    FieldOfStudy: '<http://academic.microsoft.com/FieldOfStudy>',
+    parentFieldOfStudy: '<http://academic.microsoft.com/parentFieldOfStudy>',
+    childFieldOfStudy: '<http://academic.microsoft.com/childFieldOfStudy>'
+  }
+}
+
 
 global['topictree'] = topictree;
 
-let limit = 10;
+let limit = 10; // Infinity;
 let i = 0;
 
 function mapExercise(exercise) {
@@ -52,5 +110,24 @@ function reduceChildren(memo, child) {
 
 const results = topictree.children.reduce(reduceChildren, {});
 
-console.log(Object.keys(results).length);
-console.log(results);
+const quads = Object.keys(results).reduce((memo, key) => {
+  const id = `<https://www.youtube.com/watch?v=${results[key].youtube_id}>`;
+  return [
+    ...memo,
+    { subject: id, predicate: Context.type, object: Context.Movie},
+  ]
+}, []);
+
+quads.reduce((memo, quad) => {
+  return memo.then(() => {
+    return queue.add(() => {
+      console.log('hello!');
+      return send({ type: 'write', quad });
+    });
+  });
+}, Promise.resolve()).then(() => {
+  console.log('done!');
+});
+
+
+// console.log(quads);
